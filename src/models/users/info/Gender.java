b3ef7aaac54f9a75dd93c7e1e2375c4dd58033ac
Max Barker <max@the-barkers.com>
2019-12-31 13:43:05 +0000

    Implemented User Info models
    These models store particular details about the user.

---
+++ src/models/users/info/Gender.java
@@ -0,0 +1,9 @@
+package models.users.info;
+
+/**
+ * An enumeration representing the possible genders in the system.
+ */
+public enum Gender {
+    MALE,
+    FEMALE;
+}
---
+++ src/models/users/info/ID.java
@@ -0,0 +1,51 @@
+package models.users.info;
+
+import models.I_Printable;
+
+/**
+ * A class that encapsulates the unique ID of a user.
+ */
+public class ID
+        implements I_Printable {
+    private static final int ID_LENGTH = 4;
+
+    private final Role _role;
+    private final String _idNumber;
+
+    /**
+     * Creates a ID object.
+     *
+     * @param role the initial character of the ID that identifies the user to belong to a specific group.
+     * @param idNumber a String of numbers.
+     * @throws IllegalArgumentException if length of idNumber is greater than the constant ID_LENGTH.
+     */
+    public ID(Role role, String idNumber){
+        if(idNumber.length() > ID_LENGTH)
+            throw new IllegalArgumentException(String.format("Length of idNumber is too great. Must be less than %d integers.", ID_LENGTH));
+
+        _role = role;
+        _idNumber = idNumber;
+    }
+
+    /**
+     * @return the _role variable.
+     */
+    public Role getRole() {
+        return _role;
+    }
+
+    /**
+     * @return the _idNumber variable.
+     */
+    public String getIdNumber() {
+        return _idNumber;
+    }
+
+    /**
+     * @return the object as a string variable.
+     */
+    @Override
+    public String toString(){
+        return _role + _idNumber;
+    }
+}
